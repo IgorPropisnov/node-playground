50f011136a86f120ad8ac08b0c85c1a22c87bad4
Igor Propisnov <i.propisnov@dobosolution.de>
2019-07-18 21:07:31 +0200

    playing around with simple node module exports

---
+++ node-simple-module/circle.js
@@ -0,0 +1,9 @@
+const PI = Math.PI;
+
+exports.area = (r) => {
+    return PI * r * r;
+}
+
+exports.circumference = (r) => {
+    return 2 * PI * r;
+}
---
+++ node-simple-module/index.js
@@ -0,0 +1,6 @@
+// node is able to require .js .json and .node files without the datatype suffix
+const circle = require('./circle');
+ //?
+
+circle.area(2) //?
+circle.circumference(4) //?
